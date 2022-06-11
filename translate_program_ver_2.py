dee454b5e2d5290db02e74d52704ceda07ab1512
unknown <crazystarcoder@gmail.com>
2022-06-12 01:14:41 +0900

    finall

--- translate_program_ver_2.py
+++ translate_program_ver_2.py
@@ -1,4 +1,5 @@
 
+from distutils.log import error
 from flask import Flask, request, jsonify
 import pymysql
 from codon_forupgrade import translating
@@ -21,27 +22,49 @@ app = Flask(__name__)
 sql = """SELECT * FROM USER"""
 cur.execute(sql)
 M = cur.fetchall()
-USER = {}   
+
+
 #post
 @app.route('/insertinfo', methods=['POST']) 
 def post_userinfo():
     request_data = request.form
-    id = len(USER)+1
     user_mRNA = request_data['user_mRNA']
 
+    
     user_id = request_data["user_id"]
     translated = translating(user_mRNA)
-    
-    print(user_mRNA)
+
+    error_mRNA = True
+    error_translate = True
+
+
+    if translating(user_mRNA) == 'mRNA 염기서열이 아닙니다.' :
+        user_mRNA = 'null'
+        error_mRNA = False
+
+    if translating(user_mRNA) == '개시코돈이 존재하지 않습니다.' :
+        translated = 'null'
+        error_translate = False
 
     sql = """INSERT INTO USER VALUES('%s','%s','%s')"""%(user_id, user_mRNA, translated)
     cur.execute(sql)
     portfolio_db.commit()
     
-    return jsonify()
-
-
-
+    if error_mRNA == False:
+        return jsonify({
+            "success" : "Fail",
+            "response" : "mRNA염기서열이 아닙니다."
+        })
+    elif error_translate == False :
+        return jsonify({
+            "success" : "Fail",
+            "error_name" : "개시코돈이 존재하지 않습니다"
+        })
+    else :
+        return jsonify({
+            "success" : "success",
+            "response" : "success"
+        })
 @app.route('/')
 def mainstring() :
     return '31310 박광성 - 생명II 발표용 서버'
